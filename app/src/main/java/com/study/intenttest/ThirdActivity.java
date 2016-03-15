ad5f005bc799b0678e0c752a983f1eb065888fb2
WXB506 <1922686887@qq.com>
2016-03-15 23:06:31 +0800

    use intent start activity study

--- app/src/main/java/com/study/intenttest/ThirdActivity.java
+++ app/src/main/java/com/study/intenttest/ThirdActivity.java
@@ -1,16 +1,32 @@
 package com.study.intenttest;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by WXB506 on 2016/3/15.
  */
 public class ThirdActivity extends Activity {
 
+    Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.third_layout);
+
+        button = (Button) findViewById(R.id.third_btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:10086"));
+                startActivity(intent);
+            }
+        });
     }
 }
