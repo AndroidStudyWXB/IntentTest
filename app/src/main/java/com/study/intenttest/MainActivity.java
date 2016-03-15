ea576c4f0d20bb571d5de87a05c4b9d72cbdb97f
WXB506 <1922686887@qq.com>
2016-03-15 23:01:41 +0800

    intent test

--- app/src/main/java/com/study/intenttest/MainActivity.java
+++ app/src/main/java/com/study/intenttest/MainActivity.java
@@ -1,6 +1,7 @@
 package com.study.intenttest;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,6 +10,7 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     private Button button;
+    private Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +27,19 @@ public class MainActivity extends AppCompatActivity {
                 // category的子集，才能启动某个活动
                 intent.addCategory("com.study.intenttest.MY_CATEGORY");
                 // startActivity()方法会自动在intent添加default category
+                startActivity(intent);
+            }
+        });
+
+        button2 = (Button) findViewById(R.id.button_3);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Intent.view 是一个android系统内置的动作，常量值为android.intent.action.VIEW
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                // 指明intent正在操作的数据，只有<data>标签中指定的内容和intent携带的Data完全一致时，
+                // 当前活动才能响应这个intent
+                intent.setData(Uri.parse("http:www.baidu.com"));
                 startActivity(intent);
             }
         });
---
+++ app/src/main/java/com/study/intenttest/ThirdActivity.java
@@ -0,0 +1,16 @@
+package com.study.intenttest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by WXB506 on 2016/3/15.
+ */
+public class ThirdActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.third_layout);
+    }
+}
